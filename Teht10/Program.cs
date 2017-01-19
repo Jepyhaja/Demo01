1886bca01ab35f9fa8c4dbac7ae73b898edd38aa
Jere <H9175@student.jamk.fi>
2017-01-19 15:29:00 +0200

    10 valmis , 11 alkua.

--- Teht10/Program.cs
+++ Teht10/Program.cs
@@ -10,6 +10,16 @@ namespace Teht10
     {
         static void Main(string[] args)
         {
+
+            int[] luvut = { 1, 2, 33, 44, 55, 68, 77, 96, 100 };
+            for (int i = 0; i < 9; i++)
+            {
+                if (luvut[i] % 2 == 0)
+                {
+                    Console.WriteLine("YEEEEEAAAP!!! " + luvut[i]);
+                }
+
+            }
         }
     }
 }
---
+++ Teht11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 2; i++)
+                for (int j = 0; j < 3; j++)
+                {
+
+                    Console.Write(
+                    
+                }
+
+           
+
+        }
+    }
+}
