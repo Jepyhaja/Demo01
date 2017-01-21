78d63aa07fc0059cd2a4c3f057a755880390161a
Jere <jepyhaja@gmail.com>
2017-01-21 09:56:40 +0200

    11 valmis 12 kesken(onglemia).

--- Teht11/Program.cs
+++ Teht11/Program.cs
@@ -6,19 +6,27 @@ using System.Threading.Tasks;
 
 namespace Teht11
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 2; i++)
-                for (int j = 0; j < 3; j++)
+            int luku;
+
+            Console.WriteLine("Anna luku > ");
+            string line = Console.ReadLine();
+            luku = int.Parse(line);
+
+            for (int i = 0; i < luku; i++)  // Luku määrittää kierrosten määrän. Lopuksi lisätään i muuttujaan yksi. i muuttujalla määrätään rivi.
+            {
+                for (int j = 0; j <= i; j++)  //  looppi kiertää tasan i muuttujan arvon verran. j muutujalla määrätään merkkien määrä.
                 {
 
-                    Console.Write(
-                    
-                }
+                    Console.Write('*'); //Write tulostaa peräkkäisiä * merkkejä.
 
-           
+                }
+                Console.WriteLine(' '); //kun tähdet on tulostettu i loopin loppuun vielä rivin vaihto.
+            }
 
         }
     }
---
+++ Teht12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luvut;
+            
+
+            for (int i=0; i<5; i++)
+            {
+                Console.WriteLine("Anna kokonaisluku");
+                string line = Console.ReadLine();
+                
+
+
+            }
+
+        }
+    }
+}
