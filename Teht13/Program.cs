6f7a11d1ec0be9720c49619036679e29977d28ea
Jepyhaja <Jepyhaja@gmail.com>
2017-01-24 17:30:15 +0200

    13 ja 14 kesken

---
+++ Teht13/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku, sija;
+            int[] luvut = new int[5];
+
+            for (int i = 0; i <= 4; i++) // laitetaan lukujen kyselylle looppi pyörii 5 kertaa.
+            {
+                Console.WriteLine("Anna kokonaisluku"); //haetaan luvut
+                string line = Console.ReadLine();
+                luku = int.Parse(line);                 //parsitaan kokonaisluvuksi
+                luvut[i] = luku;                        //sijoitetaan kokonaisluku luvut[i] lokeroon, i määrittyy loopissa.
+            }
+
+            // järjestetään luvut suuruus järjestykseen
+
+            for(int j=1; j<=4;j++) // aloitetaan taulukon alusta
+            {
+                if (luvut[j] > luvut[j++]) // jos [0] on suurempi kuin [1] vaihda paikkoja
+                {
+                    sija = luvut[j++];
+                    luvut[j++] = luvut[j];
+                    luvut[j] = sija;
+                }
+
+                
+            }
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.WriteLine(luvut[i]);
+            }
+        }
+    }
+}
---
+++ Teht14/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku;
+
+            Console.WriteLine("Anna luku > ");
+            string line = Console.ReadLine();
+            luku = int.Parse(line);
+
+            for (int i = 0; i < luku; i++)  // Luku määrittää kierrosten määrän. Lopuksi lisätään i muuttujaan yksi. i muuttujalla määrätään rivi.
+            {
+                for (int j = 0; j <= i; j++)  //  looppi kiertää tasan i muuttujan arvon verran. j muutujalla määrätään merkkien määrä.
+                {
+
+                    Console.Write('*'); //Write tulostaa peräkkäisiä * merkkejä.
+
+                }
+                Console.WriteLine(' '); //kun tähdet on tulostettu i loopin loppuun vielä rivin vaihto.
+            }
+        }
+    }
+}
