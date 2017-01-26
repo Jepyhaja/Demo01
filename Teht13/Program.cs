676572daf3f8a60433f3c7b0fbab913fe1a3c218
Jere <H9175@student.jamk.fi>
2017-01-26 12:24:56 +0200

    13 valmis

--- Teht13/Program.cs
+++ Teht13/Program.cs
@@ -15,29 +15,23 @@ namespace Teht13
 
             for (int i = 0; i <= 4; i++) // laitetaan lukujen kyselylle looppi pyörii 5 kertaa.
             {
-                Console.WriteLine("Anna kokonaisluku"); //haetaan luvut
+                Console.WriteLine("Anna arvosana"); //pyydetään syöttämään arvosana
                 string line = Console.ReadLine();
                 luku = int.Parse(line);                 //parsitaan kokonaisluvuksi
                 luvut[i] = luku;                        //sijoitetaan kokonaisluku luvut[i] lokeroon, i määrittyy loopissa.
             }
 
-            // järjestetään luvut suuruus järjestykseen
+          
 
-            for(int j=1; j<=4;j++) // aloitetaan taulukon alusta
-            {
-                if (luvut[j] > luvut[j++]) // jos [0] on suurempi kuin [1] vaihda paikkoja
-                {
-                    sija = luvut[j++];
-                    luvut[j++] = luvut[j];
-                    luvut[j] = sija;
-                }
+            Console.Write("total points are = " + (luvut.Sum() - luvut.Max() - luvut.Min()) + " ");
 
-                
-            }
-            for (int i = 0; i <= 4; i++)
-            {
-                Console.WriteLine(luvut[i]);
-            }
+
         }
+
+
+
+
+
+
     }
 }
